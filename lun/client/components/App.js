7493206939e597447b5fe45901f9e483dde97ff5
Gastonovich <oleksndrkorn@gmail.com>
2018-08-29 20:36:11 +0300

    fix

--- lun/client/components/App.js
+++ lun/client/components/App.js
@@ -9,16 +9,18 @@ class App extends Component {
             cards: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
             sequence:false,
             initial:true,
-            touchDirection:'',
+            touchDirection: '',
             xDown:null,
             yDown:null
         };
-        this.onTouchStart=this.onTouchStart.bind(this);
-        this.onTouchMove=this.onTouchMove.bind(this);
-        this.onKeyDown=this.onKeyDown.bind(this);
-        this.onTouchEnd=this.onTouchEnd.bind(this);
+        this.onTouchStart = this.onTouchStart.bind(this);
+        this.onTouchMove = this.onTouchMove.bind(this);
+        this.onTouchEnd = this.onTouchEnd.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+        this.Shuffle = this.Shuffle.bind(this);
+
     }
-    Shuffle(){
+    Shuffle()  {
         let a = this.state.cards;
         this.setState({sequence:false});
         for (let i = a.length - 1; i > 0; i--) {
@@ -28,35 +30,30 @@ class App extends Component {
         this.setState({cards:a,
             initial:false
         });
-    }
+    };
 
     onTouchMove(e){
-        console.log('1');
         if ( ! this.state.xDown || ! this.state.yDown ) {
-            return;
+            return
         }
-        console.log('1');
         let xUp = e.touches[0].clientX;
         let yUp = e.touches[0].clientY;
 
         let xDiff = this.state.xDown - xUp;
         let yDiff = this.state.yDown - yUp;
         let direction = null;
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
             if ( xDiff > 0 ) {
-                direction='left'
+                direction = 'left'
             } else {
-                /* right swipe */
-                direction='right'
+                direction = 'right'
             }
         }
         else {
             if ( yDiff > 0 ) {
-                /* up swipe */
-                direction='up'
+                direction = 'up'
             } else {
-                /* down swipe */
-                direction='down'
+                direction = 'down'
             }
         }
         /* reset values */
@@ -65,15 +62,12 @@ class App extends Component {
             yDown:null,
             direction:direction
         });
-        console.log(this.state.direction);
     }
     onTouchStart(e){
-        console.log('start');
         this.setState({
             xDown:e.touches[0].clientX,
             yDown:e.touches[0].clientY
         });
-        console.log(this.state.xDown ,this.state.yDown);
     }
     onTouchEnd(e){
         if(!this.state.initial) {
@@ -119,31 +113,31 @@ class App extends Component {
     }
 
     onKeyDown(event){
-        if(this.state.sequence===false && this.state.initial===false && (event.key === 'ArrowDown'|| event.key === 'ArrowUp'|| event.key === 'ArrowLeft'|| event.key === 'ArrowRight')  ) {
+        if(!this.state.sequence && !this.state.initial && (event.key === 'ArrowDown' || event.key === 'ArrowUp' || event.key === 'ArrowLeft' || event.key === 'ArrowRight')  ) {
             event.preventDefault();
             console.log('onKeyDown');
             let arr = this.state.cards;
             let a = this.state.cards.indexOf(16);
             switch(event.key) {
-                case  'ArrowDown':
+                case  'ArrowUp':
                     if(!(a>=12 && a<=15)) {
                         arr[a] = this.state.cards[a + 4];
                         arr[a + 4] = 16;
                     }
                     break;
-                case  'ArrowUp':
-                    if(!(a>=0&&a<=3)) {
+                case  'ArrowDown':
+                    if(!(a>=0 && a<=3)) {
                         arr[a] = this.state.cards[a - 4];
                         arr[a - 4] = 16;
                     }
                     break;
-                case  'ArrowLeft':
-                    if(a%4!==0) {
+                case  'ArrowRight':
+                    if(a%4 !== 0) {
                         arr.splice(a - 1, 0, arr.splice(a, 1)[0]);
                     }
                     break;
-                case 'ArrowRight':
-                    if((a+1)%4!==0 ) {
+                case 'ArrowLeft':
+                    if((a+1)%4 !== 0 ) {
                         arr.splice(a + 1, 0, arr.splice(a, 1)[0]);
                     }
                     break;
@@ -155,7 +149,7 @@ class App extends Component {
                     isSequence=false;
                     break;
                 }
-                isSequence=true
+                isSequence = true;
             }
             if(isSequence){
                 this.setState({sequence: true});
@@ -167,9 +161,15 @@ class App extends Component {
     render() {
         return (
             <div className="App" onTouchMove={this.onTouchMove} onTouchStart={this.onTouchStart} onKeyDown={this.onKeyDown} onTouchEnd={this.onTouchEnd}>
-                {this.state.sequence!==true ?
-                    <div className="gameField"> { GetCardView(this.state.cards)}</div>:<section> <h1>Thank you for a game!</h1></section>}
-                <button onClick={this.Shuffle.bind(this)}>{this.state.sequence!==true ? 'Shuffle' : 'Restart'}</button>
+                {!this.state.sequence ?
+                    <div className="gameField">
+                        {GetCardView(this.state.cards)}
+                        </div>:
+                    <section>
+                        <h1>Thank you for a game!</h1>
+                    </section>
+                }
+                <button onClick={this.Shuffle}> {!this.state.sequence ? "Shuffle" : "Restart"}</button>
             </div>
         );
     }
